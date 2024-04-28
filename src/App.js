05700bec3480fb44c301672aec9acdb82dff5afd
David Allakedet <allakedetdavid@gmail.com>
2024-04-28 23:56:56 +0000

    commit 1

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import FormeEquipe from './FormeEquipe';
+import './FormeEquipe.css';
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [nomEquipe1, setNomEquipe1] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [nomEquipe2, setNomEquipe2] = useState('');
+  // eslint-disable-next-line no-unused-vars
+  const [scoresEquipe1, setScoresEquipe1] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [scoresEquipe2, setScoresEquipe2] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [predictionReady, setPredictionReady] = useState(false);
+
+  const handleScoreSubmission = (nomEquipe, scores) => {
+    if (!nomEquipe1) {
+      setNomEquipe1(nomEquipe);
+      setScoresEquipe1(scores);
+    } else {
+      setNomEquipe2(nomEquipe);
+      setScoresEquipe2(scores);
+      setPredictionReady(true);
+    }
+  };
+
+  return (
+    <div >
+      <FormeEquipe onScoreSubmit={handleScoreSubmission} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FormeEquipe.js
@@ -0,0 +1,223 @@
+import React, { useState } from 'react';
+import './FormeEquipe.css';
+
+
+function FormeEquipe({ onScoreSubmit }) {
+  const [equipe1, setEquipe1] = useState({
+    nom: '',
+    scores: [],
+    butM: '',
+    butC: '',
+  });
+
+  const [equipe2, setEquipe2] = useState({
+    nom: '',
+    scores: [],
+    butM: '',
+    butC: '',
+  });
+
+  const [totalScoreEquipe1, setTotalScoreEquipe1] = useState(0);
+  const [totalScoreEquipe2, setTotalScoreEquipe2] = useState(0);
+  const [goalAverageEquipe1, setGoalAverageEquipe1] = useState(0);
+  const [goalAverageEquipe2, setGoalAverageEquipe2] = useState(0);
+  const [calculsEffectues, setCalculsEffectues] = useState(false);
+  const [equipeFavorite, setEquipeFavorite] = useState('');
+
+  const handleTeamNameChange = (e, equipeNum) => {
+    const newNom = e.target.value;
+    if (equipeNum === 1) {
+      setEquipe1({ ...equipe1, nom: newNom });
+    } else {
+      setEquipe2({ ...equipe2, nom: newNom });
+    }
+  };
+
+  const handleAddScore = (e, equipeNum) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const butM = parseInt(formData.get('butM'), 10);
+    const butC = parseInt(formData.get('butC'), 10);
+    const newScore = { butM, butC };
+    if (equipeNum === 1) {
+      setEquipe1({
+        ...equipe1,
+        scores: [...equipe1.scores, newScore],
+        butM: '',
+        butC: '',
+      });
+    } else {
+      setEquipe2({
+        ...equipe2,
+        scores: [...equipe2.scores, newScore],
+        butM: '',
+        butC: '',
+      });
+    }
+    if (equipe1.scores.length > 0 && equipe2.scores.length > 0) {
+      onScoreSubmit(1, equipe1.nom, equipe1.scores);
+      onScoreSubmit(2, equipe2.nom, equipe2.scores);
+    }
+    // Calculer et mettre à jour les scores totaux
+    const totalScore1 = equipe1.scores.reduce((acc, cur) => acc + cur.butM, 0);
+    const totalScore2 = equipe2.scores.reduce((acc, cur) => acc + cur.butM, 0);
+    setTotalScoreEquipe1(totalScore1);
+    setTotalScoreEquipe2(totalScore2);
+  };
+  
+  const handleCalculate = () => {
+    if (equipe1.scores.length === 5 && equipe2.scores.length === 5) {
+      onScoreSubmit(1, equipe1.nom, equipe1.scores);
+      onScoreSubmit(2, equipe2.nom, equipe2.scores);
+  
+       // Calculer et mettre à jour le goal average
+    const goalAverage1 = calculateGoalAverage(equipe1);
+    const goalAverage2 = calculateGoalAverage(equipe2);
+    setGoalAverageEquipe1(goalAverage1);
+    setGoalAverageEquipe2(goalAverage2);
+
+      // Calculer et mettre à jour les scores totaux
+      const totalScore1 = equipe1.scores.reduce((acc, cur) => acc + cur.butM, 0);
+      const totalScore2 = equipe2.scores.reduce((acc, cur) => acc + cur.butM, 0);
+      setTotalScoreEquipe1(totalScore1);
+      setTotalScoreEquipe2(totalScore2);
+  
+      // Déterminer l'équipe favorite
+      if (goalAverage1 > goalAverage2) {
+        setEquipeFavorite(equipe1.nom);
+      } else if (goalAverage1 < goalAverage2) {
+        setEquipeFavorite(equipe2.nom);
+      } else {
+        setEquipeFavorite("Les deux équipes vont faire un jeu égal");
+      }
+
+      // Réinitialiser la liste des scores
+    setEquipe1({ ...equipe1, scores: [] });
+    setEquipe2({ ...equipe2, scores: [] });
+    // Réinitialiser l'état de calcul effectué
+    setCalculsEffectues(true);
+  
+      setCalculsEffectues(true);
+  
+    } else {
+      alert("Veuillez entrer les résultats des cinq derniers matchs pour chaque équipe pour poursuivre.");
+    }
+  };
+  
+  const calculateGoalAverage = (equipe) => {
+    const totalButM = equipe.scores.reduce((total, score) => total + score.butM, 0);
+    const totalButC = equipe.scores.reduce((total, score) => total + score.butC, 0);
+    return totalButM - totalButC;
+  };
+  
+  return (
+    
+    <div className='form-container'>
+      <h1>Predictions Matchs</h1>
+      <div className='team'>
+        <h2>Équipe 1</h2>
+        <form onSubmit={(e) => handleAddScore(e, 1)} className='team-form'>
+          <input
+            type="text"
+            placeholder="Nom de l'équipe 1"
+            value={equipe1.nom}
+            onChange={(e) => handleTeamNameChange(e, 1)}
+            className='input-field'
+          />
+          <div className='score-inputs'>
+            <input
+              type="number"
+              name="butM"
+              placeholder="Buts marqués"
+              value={equipe1.butM}
+              onChange={(e) => setEquipe1({ ...equipe1, butM: parseInt(e.target.value, 10) })}
+              className='input-field'
+            />
+            <input
+              type="number"
+              name="butC"
+              placeholder="Buts encaissés"
+              value={equipe1.butC}
+              onChange={(e) => setEquipe1({ ...equipe1, butC: parseInt(e.target.value, 10) })}
+              className='input-field'
+            />
+          </div>
+          <button type="submit" className='submit-button'>Ajouter Score</button>
+        </form>
+        <h3 className='team-heading'>Scores {equipe1.nom}</h3>
+        <ul className='score-list'>
+          {equipe1.scores.map((score, index) => (
+            <li key={index} className='score-item'>{score.butM}  -  {score.butC}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className='team'>
+        <h2>Équipe 2</h2>
+        <form onSubmit={(e) => handleAddScore(e, 2)} className='team-form'>
+          <input
+            type="text"
+            placeholder="Nom de l'équipe 2"
+            value={equipe2.nom}
+            onChange={(e) => handleTeamNameChange(e, 2)}
+            className='input-field'
+          />
+          <div className='score-inputs'>
+            <input
+              type="number"
+              name="butM"
+              placeholder="Buts marqués"
+              value={equipe2.butM}
+              onChange={(e) => setEquipe2({ ...equipe2, butM: parseInt(e.target.value, 10) })}
+              className='input-field'
+            />
+            <input
+              type="number"
+              name="butC"
+              placeholder="Buts encaissés"
+              value={equipe2.butC}
+              onChange={(e) => setEquipe2({ ...equipe2, butC: parseInt(e.target.value, 10) })}
+              className='input-field'
+            />
+          </div>
+          <button type="submit" className='submit-button'>Ajouter Score</button>
+        </form>
+        <h3 className='team-heading'>Scores {equipe2.nom}</h3>
+        <ul className='score-list'>
+          {equipe2.scores.map((score, index) => (
+            <li key={index} className='score-item'>{score.butM}  -  {score.butC}</li>
+          ))}
+        </ul>
+      </div>
+      <div className='calculate'>
+        <button onClick={handleCalculate} className='calculate-button'>Calculer</button>
+      </div>
+      
+      {calculsEffectues && (
+        <div className='results-container'>
+          <h3>Résultats :</h3>
+          <p>
+          <strong>{equipe1.nom}</strong> Totalise : {totalScoreEquipe1} buts marqués et a un Goal Average de :{' '}
+            {goalAverageEquipe1} buts
+          </p>
+          <p>
+            <strong>{equipe2.nom}</strong> Totalise : {totalScoreEquipe2} buts marqués et a un Goal Average de :{' '}
+            {goalAverageEquipe2} buts
+          </p>
+          <h2>Prediction</h2>
+          
+          <p className='prediction-text'>{equipeFavorite !== 'Les deux équipes vont faire un jeu égal' ? (<>L'équipe favorite pour gagner le prochain match est : <strong>{equipeFavorite}</strong></>
+          ) : (
+            <>Il y aura égalité entre les deux équipes pour le prochain match.</>
+          )}
+        </p>
+
+
+        </div>
+      )}
+    </div>
+  );
+  
+}
+
+export default FormeEquipe;
